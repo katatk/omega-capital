682af8a3312e7abbf8be5de335c95831a3445653
Kat <katatk@users.noreply.github.com>
2017-11-08 14:48:06 +1300

    pretty much done, just some minor differences in mobile dropdown menu on live site

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-document.DOMContentLoaded
+$(document).ready(function () {
+
+    // scroll to top function
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+
+    $('#back-to-top').click(function () {
+        // first parameter is the property to be animated
+        // second if duration in milliseconds
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
+    // mobile menu animate function
+    $('.navbar-toggler-icon').click(function () {
+        $(this).toggleClass('open');
+    });
+
+     // slidedown effect for main menu
+    /*$('.navbar-toggler').click(function () {
+        $('.navbar-collapse').slideToggle(500, function () {
+
+        });
+    });*/
+
+    // slidedown effect for dropdown menu
+    $('.dropdown-toggle').click(function () {
+        $('.dropdown-menu').slideToggle();
+    });
+
+});
